cc662b200e8eda5ca0ad965f67b29a1b7bad557b
Josh Beaver <josh.elvn11@gmail.com>
2024-02-29 19:27:04 +0000

    added sumernote config

--- vocab/admin.py
+++ vocab/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Word, Word_Set, Set_Word_Junction
 
 # Register your models here.
--- vocab/views.py
+++ vocab/views.py
@@ -3,6 +3,6 @@ from .models import Word_Set
 
 # Create your views here.
 def set_list(request):
-    word_sets = Word_Set.objects.all()
+    word_sets = Word_Set.objects.all().order_by("set_order")
 
-    return render(request, 'vocab/word-sets.html', {'word_sets': word_sets})
+    return render(request, "vocab/word-sets.html", {"word_sets": word_sets})
