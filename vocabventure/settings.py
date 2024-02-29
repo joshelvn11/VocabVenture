0b80d6a3e1ef14b386a0a663a5d3add0642d00d2
Josh Beaver <josh.elvn11@gmail.com>
2024-02-29 21:52:07 +0000

    enchanced base.html with navbar

--- vocabventure/settings.py
+++ vocabventure/settings.py
@@ -129,7 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Base directory where your global static files are
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Collect static to here for production
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
