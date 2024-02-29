7f4eeabc3a619874f896e16b95a2f8f646c76878
Josh Beaver <138677896+joshelvn11@users.noreply.github.com>
2024-02-29 13:34:39 +0000

    removed app.vocabventure

--- app.vocabventure/vocabventure/env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgres://vocabventure_user:mWLX03wIfQQFcQaKxzXrWVvVLjMfl9GV@dpg-cnf61f5a73kc73919tm0-a.frankfurt-postgres.render.com/vocabventure")
