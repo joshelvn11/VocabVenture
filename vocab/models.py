197b022866bc4b25c9e3f5d736c618386e5f6772
Josh Beaver <josh.elvn11@gmail.com>
2024-02-29 17:30:10 +0000

    added meta info to models

--- vocab/models.py
+++ vocab/models.py
@@ -10,11 +10,17 @@ class Word(models.Model):
     word_roman = models.CharField(max_length=100)
     word_pronounciation = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"[ID] {self.word_id} [UKR] {self.word_ukrainian} [ENG] {self.word_english}"
+
 # Model to define sets of words
 class Word_Set(models.Model):
     set_id = models.IntegerField(unique=True)
     set_title = models.CharField(max_length=100)
     set_slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"[ID] {self.set_id} [TITLE] {self.set_title}"
 
 # Junction table to define which words belong to which sets
 # Junction table needed as words can belong to multiple sets 
@@ -22,6 +28,12 @@ class Word_Set(models.Model):
 # and words can have multiple sets
 class Set_Word_Junction(models.Model):
     word = models.ForeignKey(Word, on_delete=models.PROTECT)
-    word_set = models.ForeignKey(Word_Set, on_delete=models.PROTECT) 
+    word_set = models.ForeignKey(Word_Set, on_delete=models.PROTECT)
+
+    class Meta:
+        ordering = ["word"] 
+
+    def __str__(self):
+        return f"[WORD] {self.word} [SET] {self.word}"
 
 
