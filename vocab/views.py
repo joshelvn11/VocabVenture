7d228f195a619958bd05f2f1dddd350309137101
Josh Beaver <josh.elvn11@gmail.com>
2024-03-01 22:59:28 +0000

    fixed get_word_list api endpoint

--- vocab/views.py
+++ vocab/views.py
@@ -18,6 +18,28 @@ def word_list(request):
     return render(request, "vocab/word-list.html", {"words": words})
 
 def get_word_list(request):
+    # Retrieve the query set
     words = Word.objects.all()
 
-    return JsonResponse(words)
+    # Create a dictionary for sending
+    data = []
+
+    for word in words:
+        word_data = {
+            "word_ukrainian": word.word_ukrainian,
+            "word_english": word.word_english,
+            "word_roman": word.word_roman,
+            "word_gender": word.word_gender,
+            "word_pronounciation": word.word_pronounciation,
+            "word_explanation": word.word_explanation,
+            "word_examples": word.word_examples,
+        }
+
+        data.append(word_data)
+
+    data_response = {
+        "message": "Success!",
+        "data": data,
+    }
+
+    return JsonResponse(data_response)
