2462aa37069d75d112feca55bc8c29bde09e41d2
Josh Beaver <josh.elvn11@gmail.com>
2024-03-02 11:04:52 +0000

    created basic words list table loaded from db/api

--- static/js/words-list.js
+++ static/js/words-list.js
@@ -1,4 +1,10 @@
-fetch("api/words/list")
+// Get the words list table
+const wordsListTable = $("#words-list-table");
+
+let wordData;
+
+// Fetch the word data from the api
+fetch("http://127.0.0.1:8000/api/words/list")
   .then((response) => {
     if (!response.ok) {
       console.log("Error in response");
@@ -6,5 +12,21 @@ fetch("api/words/list")
     return response.json(); // Parse the response body as JSON
   })
   .then((data) => {
-    console(data); // Process the data
+    wordData = data.data;
+  })
+  .then((data) => {
+    // Iterate over the data and add a row for every element in the data
+    console.log(wordData);
+    for (let word of wordData) {
+      console.log(word);
+      wordsListTable.append(
+        `<tr> 
+        <td>${word["word_ukrainian"]}</td> 
+        <td>${word["word_english"]}</td> 
+        <td>${word["word_roman"]}</td> 
+        <td>${word["word_pronounciation"]}</td>
+        <td></td>
+        </tr>`
+      );
+    }
   });
