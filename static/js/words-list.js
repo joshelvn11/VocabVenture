9ae1beecfc0247930dc19117311b8457d8a9110d
Josh Beaver <josh.elvn11@gmail.com>
2024-03-18 13:21:32 +0000

    [FIX] fixed local server URL variable

--- static/js/words-list.js
+++ static/js/words-list.js
@@ -2,7 +2,7 @@ const devMode = true;
 let serverURL;
 
 if (devMode) {
-  serverURL = "http://127.0.0.1:800";
+  serverURL = "http://127.0.0.1:8000";
 } else {
   serverURL = "https://vocabventure.onrender.com";
 }
