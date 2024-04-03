91bfb68c285b2b90c6cce50bc29bf7c526146e18
Josh Beaver <josh.elvn11@gmail.com>
2024-04-03 15:07:54 +0100

    [MISC] delete service worker caches

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -5,3 +5,15 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
