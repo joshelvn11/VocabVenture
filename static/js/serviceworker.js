f1e086d0fa143adb177640249822e8b16a906ca6
Josh Beaver <josh.elvn11@gmail.com>
2024-04-20 13:12:01 +0100

    [IMPROVE] removed  cache deleting service worker code

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -5,15 +5,3 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-});
